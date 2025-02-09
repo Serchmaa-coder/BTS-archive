0fed35a84c46cbc28b90e9ada16d6c017ac05e3e
Serchmaa <minsera1029@gmail.com>
2025-02-10 00:41:52 +0800

    a

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,17 @@ import Image from "next/image";
 import BtsLogoOutlined from "../public/bts_logo_outlined.png";
 import { Button } from "./ui/button";
 
-export default function Header(){
-    return (
-        <div className="mr-10 ml-10 flex flex-row justify-between">
-        <div className="text-5xl text-purple-800">
-          <Image src={BtsLogoOutlined} alt="BTS logo" height={70} />
-        </div>
-        <div className="grid grid-cols-3 gap-x-2">
-<Button > Read more</Button>        <Button > Read more</Button>     <Button > Read more</Button>     
-        </div>
+export default function Header() {
+  return (
+    <div className="mr-10 ml-10 flex flex-row justify-between">
+      <div className="text-5xl text-purple-800">
+        <Image src={BtsLogoOutlined} alt="BTS logo" height={70} />
       </div>
-    );
+      <div className="grid grid-cols-3 gap-x-2">
+        <Button>Discography</Button> 
+        <Button>Members</Button>{" "}
+        <Button>Fandom</Button>
+      </div>
+    </div>
+  );
 }
